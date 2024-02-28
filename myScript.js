9aeb47c4b0286da0fc0bf795a7ce8846d05389eb
Made <122382388+Marselllniiboyy@users.noreply.github.com>
2024-02-28 14:06:26 +0800

    fix: logic button

--- myScript.js
+++ myScript.js
@@ -11,6 +11,7 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+
 function playRound(playerSelection,computerSelection) {
     if (playerSelection === computerSelection) {
         return "Tie Game";
@@ -21,33 +22,58 @@ function playRound(playerSelection,computerSelection) {
     }
 }
 
+const rockBtn = document.querySelector("#rock")
+const paperBtn = document.querySelector("#paper")
+const scissoreBtn = document.querySelector("#scissors")
+
 function playGame(){
     let playerScore = 0;
     let computerScore = 0;
+        
+        rockBtn.addEventListener("click", ()=>{
+            const playerSelection = "rock";
+            const computerSelection = getComputerChoice();
+            eveluateRound(playerSelection,computerSelection)
+            checkScore();
+        })
 
-    
-        const playerSelection = prompt("Please choose: Rock,Paper, or scissore").toLocaleLowerCase();
-        const computerSelection = getComputerChoice();
-        if (playRound(playerSelection,computerSelection) === "Tie Game"){
-            console.log(playRound(playerSelection,computerSelection))
-        }else if (playRound(playerSelection,computerSelection) === "You win dude!" + " " + playerSelection + " beats" +" " + computerSelection) {
-            playerScore++
-            console.log(playRound(playerSelection,computerSelection));
-        }else if(playRound(playerSelection,computerSelection) === "You Lose:( dude, " + computerSelection + " " + "beets " + playerSelection){
-            computerScore++
-            console.log(playRound(playerSelection,computerSelection))
+        paperBtn.addEventListener("click", ()=>{
+            const playerSelection = "paper";
+            const computerSelection = getComputerChoice();
+            eveluateRound(playerSelection,computerSelection)
+            checkScore();
+        })
+
+        scissoreBtn.addEventListener("click", ()=>{
+            const playerSelection = "scissore";
+            const computerSelection = getComputerChoice();
+            eveluateRound(playerSelection,computerSelection)
+            checkScore();
+        })
+        
+        function eveluateRound(playerSelection,computerSelection){
+            if (playRound(playerSelection,computerSelection) === "Tie Game"){
+                console.log(playRound(playerSelection,computerSelection))
+            }else if (playRound(playerSelection,computerSelection) === "You win dude!" + " " + playerSelection + " beats" +" " + computerSelection) {
+                playerScore++ //+1 if you winn
+                console.log(playRound(playerSelection,computerSelection));
+            }else if(playRound(playerSelection,computerSelection) === "You Lose:( dude, " + computerSelection + " " + "beets " + playerSelection){
+                computerScore++
+                console.log(playRound(playerSelection,computerSelection))
+            }
         }
-    
 
-    if (playerScore > computerScore) {
-        console.log(`Congrats you win, you got ${playerScore} score and computer got ${computerScore}`)
-    } else if (playerScore < computerScore){
-        console.log(`Sorry You lose, you got ${playerScore} and computer got ${computerScore}`)
-    } else{
-        console.log(`Tie Game, you got ${playerScore} and computer got ${computerScore}`)
-    }
+        function checkScore (){
+            if (playerScore > computerScore) {
+                console.log(`Congrats you win, you got ${playerScore} score and computer got ${computerScore}`)
+            } else if (playerScore < computerScore){
+                console.log(`Sorry You lose, you got ${playerScore} and computer got ${computerScore}`)
+            } else{
+                console.log(`Tie Game, you got ${playerScore} and computer got ${computerScore}`)
+            }
+        }
 } 
 
-playGame()
+playGame();
 
 // Damm All done
